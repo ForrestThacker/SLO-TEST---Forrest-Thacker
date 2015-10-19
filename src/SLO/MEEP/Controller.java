696ebdd2c02a0d325312686644568046b5c99411
ForrestThacker <fyruskasai@gmail.com>
2015-10-19 13:32:30 -0600

    Did some work on the SLO.

---
+++ src/SLO/MEEP/Controller.java
@@ -0,0 +1,13 @@
+package SLO.MEEP;
+
+import java.util.scanner;
+public class Project
+	Scanner input;
+{
+	public Project
+}
+
+public class Controller 
+{
+	
+}
---
+++ src/SLO/MEEP/Runner.java
@@ -0,0 +1,16 @@
+package SLO.MEEP;
+
+import controller.slo.Controller;
+
+public class Runner 
+	{
+	public static void main(String[] args){
+	Controller slotController = new Controller();
+	sloController.start();
+	}
+}
+
+public class runner
+{
+	
+}
